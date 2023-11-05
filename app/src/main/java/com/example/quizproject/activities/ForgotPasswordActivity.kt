d7357757de5d89ed74c4cb7f279bc8fa566e9d57
nDjura <nemanjadjura33@gmail.com>
2023-11-05 14:01:15 +0100

    Final Comit

--- app/src/main/java/com/example/quizproject/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/quizproject/activities/ForgotPasswordActivity.kt
@@ -2,11 +2,47 @@ package com.example.quizproject.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.quizproject.R
+import android.view.View
+import android.widget.Toast
+import com.example.quizproject.databinding.ActivityForgotPasswordBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ForgotPasswordActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityForgotPasswordBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_forgot_password)
+        binding = ActivityForgotPasswordBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnResetPassword.setOnClickListener {
+            val email = binding.etEmail.text.toString().trim()
+
+            if (email.isEmpty()) {
+                Toast.makeText(applicationContext, "Please enter your email.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.sendPasswordResetEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            applicationContext,
+                            "Password reset email sent to your address.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Error sending the email.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+        }
     }
 }
